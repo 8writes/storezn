13feb06b98b9f48b6bf232efb2131c7e9109f5cf
Chisom Ozoemena <ozimmanuel79@gmail.com>
2026-08-11 17:39:20 +0100

    update

--- app/(dashboard)/super-admin/analytics/loading.js
+++ app/(dashboard)/super-admin/analytics/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex items-center justify-center py-24">
-      <span className="spinner" />
+      <span className="spinner-lg" />
     </div>
   );
 }
--- app/storefront/[host]/loading.js
+++ app/storefront/[host]/loading.js
@@ -5,7 +5,7 @@
 export default function Loading() {
   return (
     <div className="flex items-center justify-center py-24">
-      <span className="spinner" />
+      <span className="spinner-lg" />
     </div>
   );
 }
