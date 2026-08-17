9014c6469ce4abee80206958a89121ea97f5a79a
Chisom Ozoemena <ozimmanuel79@gmail.com>
2026-08-17 12:27:16 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const jakarta = Plus_Jakarta_Sans({
 });
 
 export const metadata = {
-  title: "Storezn",
+  title: "Storezn | Business management & e-commerce platform",
   description: "E-commerce platform, get your own dedicated store.",
   manifest: "/manifest.json",
   appleWebApp: {
